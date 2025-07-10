b5b8d13584c6ac381c978c36ede6b13ff7cc5c02
“[firstname <github0921@gmail.com>
2025-07-10 16:57:07 +0300

    Test/3

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://serrurierservices.fr',
+  siteUrl: 'https://www.serrurierservices.fr',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'weekly',
