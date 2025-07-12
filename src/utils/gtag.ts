42a84310a9e632c5c351882cb671b6d797d58a9a
“[firstname <github0921@gmail.com>
2025-07-12 14:57:36 +0300

    Conversii/tel2

--- src/utils/gtag.ts
+++ src/utils/gtag.ts
@@ -4,13 +4,23 @@ declare global {
     }
   }
   
-  export const handlePhoneClickConversion = () => {
+  export const handlePhoneClickConversion = (e?: React.MouseEvent<HTMLAnchorElement>) => {
+    if (e) e.preventDefault();
+    const phoneHref = 'tel:+33659514692';
     if (typeof window !== 'undefined' && typeof window.gtag !== 'undefined') {
       window.gtag('event', 'conversion', {
         send_to: 'AW-1003293596/wzxmCL-uoO8aEJyXtN4D',
         value: 75.0,
         currency: 'EUR',
+        event_callback: () => {
+          window.location.href = phoneHref;
+        }
       });
+      setTimeout(() => {
+        window.location.href = phoneHref;
+      }, 500);
+    } else {
+      window.location.href = phoneHref;
     }
   };
   
